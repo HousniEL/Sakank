31bc3027e64a2a541087c7cea2bfdbc8e4af8761
HousniEL <elaich.housni@gmail.com>
2021-07-24 13:22:35 +0100

    Filtre

---
+++ Footer/footer.php
@@ -0,0 +1,45 @@
+  <link rel="stylesheet" type="text/css" href="../Footer/footer.css">
+  
+    <footer class="page-footer font-small" style="">
+      <div class="container-fluid">
+            <div class="row">
+              <div id="blockA" class="col-md-3" style="background-color: #26966d; padding: 10px;"> 
+                <div style="text-align: center;"><img src="../logo/logo.png"></div>
+              </div>
+              <div id="blockB" class="col-md-3" style="background-color: #26966d; padding: 10px;">
+                <div style="padding: 3px;"><a href="#">Publier une annonce</a></div>
+                <div style="padding: 3px;"><a href="#">Inscrivez votre entreprise</a></div>
+                <div style="padding: 3px;"><a href="#">Blog</a></div>
+              </div>
+              <div id="blockC" class="col-md-3" style="background-color: #26966d; padding: 10px;">
+                <div style="padding: 3px;"><a href="#">Qui somme-nous?</a></div>
+                <div style="padding: 3px;"><a href="#">FAQ's</a></div>
+                <div style="padding: 3px;"><a href="#">Conditions d'utilisation</a></div>
+                <div style="padding: 3px;"><a href="#">Protection des données</a></div>
+              </div>
+              <div id="blockD" class="col-md-3" style="background-color: #26966d; padding: 10px;">
+                <div style="padding-top: 5px; padding-bottom: 5px; font-family: baloo; color: white;">Contactez-nous sur :</div>
+                    <div title="facebook">
+                      <a href="https:/www.facebook.com/" target="_blank"></a>
+                    </div>
+                    <div title="twitter">
+                      <a href="https:/www.twitter.com/" target="_blank"></a>
+                    </div>
+                    <div title="linkedin">
+                      <a href="https://www.linkedin.com/" target="_blank"></a>
+                    </div>
+                    <div title="pinterest">
+                      <a href="https:/www.pinterest.com" target="_blank"></a>
+                    </div>
+                    <div title="instagram">
+                      <a href="https:/www.instagram.com" target="_blank"></a>
+                    </div>
+              </div>
+              <div class="w-100"></div>
+              <div class="col" style="font-size: 8pt; padding: 10px; color: black; text-align: center; background-color: #5dd95d; font-family:baloo;">
+                &copy; 2020 Sakank. Tous droits réservés. 
+              </div>
+            </div>
+          </div>
+
+    </footer>
---
+++ Header/header.php
@@ -0,0 +1,92 @@
+<link rel="stylesheet" type="text/css" href="../Header/header.css">
+
+  <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #26966d; margin-bottom: 2%;">
+    <a class="navbar-brand" href="#"><img src="../logo/logo.png" alt="Sakank" width="148" height="40"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown position-static"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="#" data-display="static">Achat</a>
+              <div class="dropdown-menu w-100 top-auto colwhat">
+                  <div class="container">
+                      <div class="row w-100">
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item">Appartements</a>
+                            <a href="#" class="dropdown-item">Maison</a>
+                            <a href="#" class="dropdown-item">Studio</a>
+                            <a href="#" class="dropdown-item">Chambre</a>
+                        </div>
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item">Loft</a>
+                            <a href="#" class="dropdown-item">Villa</a>
+                            <a href="#" class="dropdown-item">Penthouse</a>
+                            <a href="#" class="dropdown-item">Riad</a>
+                        </div>
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item">Duplex</a>
+                            <a href="#" class="dropdown-item">Triplex</a>
+                            <a href="#" class="dropdown-item">Bungalow</a>
+                            <a href="#" class="dropdown-item">Terrain</a>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+          </li>
+          <li class="nav-item dropdown position-static"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="#" data-display="static">Location</a>
+            <div class="dropdown-menu w-100 top-auto colwhat">
+                <div class="container">
+                    <div class="row w-100">
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item">Appartements</a>
+                            <a href="#" class="dropdown-item">Maison</a>
+                            <a href="#" class="dropdown-item">Studio</a>
+                            <a href="#" class="dropdown-item">Chambre</a>
+                        </div>
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item">Loft</a>
+                            <a href="#" class="dropdown-item">Villa</a>
+                            <a href="#" class="dropdown-item">Penthouse</a>
+                            <a href="#" class="dropdown-item">Riad</a>
+                        </div>
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item">Duplex</a>
+                            <a href="#" class="dropdown-item">Triplex</a>
+                            <a href="#" class="dropdown-item">Bungalow</a>
+                            <a href="#" class="dropdown-item">Terrain</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item dropdown position-static"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="#" data-display="static">Professionnels</a>
+            <div class="dropdown-menu w-100 top-auto colwhat">
+                <div class="container">
+                    <div class="row w-100">
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item"><h3>Bureaux</h3></a>
+                        </div>
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item"><h3>Magasins</h3></a>
+                        </div>
+                        <div class="text-center col-sm-4">
+                            <a href="#" class="dropdown-item"><h3>Fonds de commerce</h3></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Filtre Avancé</a>
+        </li>
+          </ul>
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Login</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Register</a>
+              </li>
+          </ul>
+      </div>
+</nav>
